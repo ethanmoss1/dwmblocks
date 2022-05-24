8a86ccc6b1d186ee4e4b53b3adbe212aa783978f
ethanmoss1 <cywinskimoss@gmail.com>
2022-05-24 17:00:29 +0100

    updated spacing

--- scripts/bcklt.sh
+++ scripts/bcklt.sh
@@ -10,21 +10,29 @@ esac
 
 level=$(xbacklight -get | awk -F '.' '{ print $1 }')
 
-if [ $level -ge 66 ]
-then
+if [ $level -ge 80 ]; then
     icon=""
-elif [ $level -le 33 ]
-then
+elif [ $level -le 40 ]; then
     icon=""
 else
     icon=""
 fi
 
-iconpath="/usr/share/icons/Faba/48x48/notifications/notification-display-brightness.svg"
-iconnotify=""
+if [ $level -ge 100 ]; then
+    space=""
+else
+    if [ $level -ge 10 ]; then
+        space=" "
+    else
+        space="  "
+    fi
+fi
+
+space+="              "
+iconnotify="/usr/share/icons/Faba/48x48/notifications/notification-display-brightness.svg"
 notify-send "Brightness$space$level" \
         -h int:value:$level \
         -t 2000 \
         -r 102 \
-        -i $iconpath$iconnotify
+        -i $iconnotify
 echo "$icon"
